3208c60d4f631b6007c01d2c3b8604e82839c8e5
Pierre-Hugues Husson <phh@phh.me>
2018-05-26 00:28:46 +0200

    Don't set google experimental camera features
    Change-Id: I1622efadab178a514a18747673f93590c8ba751f

--- config/gapps.mk
+++ config/gapps.mk
@@ -1,3 +1,9 @@
+PRODUCT_COPY_FILES += \
+	vendor/omni/config/empty-permission.xml:system/etc/permissions/com.google.android.camera2.xml \
+	vendor/omni/config/empty-permission.xml:system/etc/permissions/com.google.android.camera.experimental2015.xml \
+	vendor/omni/config/empty-permission.xml:system/etc/permissions/com.google.android.camera.experimental2016.xml \
+	vendor/omni/config/empty-permission.xml:system/etc/permissions/com.google.android.camera.experimental2017.xml
+
 GAPPS_VARIANT := nano
 GAPPS_FORCE_PACKAGE_OVERRIDES := true
 GAPPS_FORCE_DIALER_OVERRIDES := true
