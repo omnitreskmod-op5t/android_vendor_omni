67dc8f64c3ca0eeccfa9409b4caeaa8a33ee4ab7
mTresk <djtresk@gmail.com>
2018-09-06 19:49:02 +0200

    Pixel 2XL default sounds
    Change-Id: If493df5db72e8cfc6515a376c54a076cd5ed776f

--- config/common.mk
+++ config/common.mk
@@ -64,26 +64,11 @@ PRODUCT_COPY_FILES += \
     vendor/omni/prebuilt/etc/permissions/privapp-permissions-omni.xml:system/etc/permissions/privapp-permissions-omni.xml \
     vendor/omni/prebuilt/etc/permissions/privapp-permissions-elgoog.xml:system/etc/permissions/privapp-permissions-elgoog.xml
 
-# custom omni sounds
+# default sounds
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    ro.config.ringtone=omni_ringtone1.ogg \
-    ro.config.notification_sound=omni_notification1.ogg \
-    ro.config.alarm_alert=omni_alarm1.ogg
-
-PRODUCT_COPY_FILES += \
-    vendor/omni/prebuilt/sounds/camera_click_48k.ogg:system/media/audio/ui/camera_click.ogg \
-    vendor/omni/prebuilt/sounds/VideoRecord_48k.ogg:system/media/audio/ui/VideoRecord.ogg \
-    vendor/omni/prebuilt/sounds/VideoStop_48k.ogg:system/media/audio/ui/VideoStop.ogg \
-    vendor/omni/prebuilt/sounds/omni_ringtone1.ogg:system/media/audio/ringtones/omni_ringtone1.ogg \
-    vendor/omni/prebuilt/sounds/omni_ringtone2.ogg:system/media/audio/ringtones/omni_ringtone2.ogg \
-    vendor/omni/prebuilt/sounds/omni_ringtone3.ogg:system/media/audio/ringtones/omni_ringtone3.ogg \
-    vendor/omni/prebuilt/sounds/omni_alarm1.ogg:system/media/audio/alarms/omni_alarm1.ogg \
-    vendor/omni/prebuilt/sounds/omni_alarm2.ogg:system/media/audio/alarms/omni_alarm2.ogg \
-    vendor/omni/prebuilt/sounds/omni_notification1.ogg:system/media/audio/notifications/omni_notification1.ogg \
-    vendor/omni/prebuilt/sounds/omni_lowbattery1.ogg:system/media/audio/ui/omni_lowbattery1.ogg \
-    vendor/omni/prebuilt/sounds/omni_lock_phone.ogg:system/media/audio/ui/omni_lock_phone.ogg \
-    vendor/omni/prebuilt/sounds/omni_unlock_phone.ogg:system/media/audio/ui/omni_unlock_phone.ogg \
-    vendor/omni/prebuilt/sounds/omni_charge_start.ogg:system/media/audio/ui/omni_charge_start.ogg
+    ro.config.ringtone=The_big_adventure.ogg \
+    ro.config.notification_sound=Beginning.ogg \
+    ro.config.alarm_alert=Bright_morning.ogg
 
 # mkshrc
 PRODUCT_COPY_FILES += \
