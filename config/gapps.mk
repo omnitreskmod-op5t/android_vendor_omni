e3b7b12f1c1f3232f6be170dc5e89658cb6f6166
mTresk <djtresk@gmail.com>
2018-10-06 19:47:03 +0200

    Add MarkupGoogle
    Change-Id: Icf49d42c5a88cc17f45d174d6665f2e91a5e7633

--- config/gapps.mk
+++ config/gapps.mk
@@ -9,6 +9,6 @@ GAPPS_FORCE_PACKAGE_OVERRIDES := true
 GAPPS_FORCE_DIALER_OVERRIDES := true
 GAPPS_FORCE_MMS_OVERRIDES := true
 GAPPS_FORCE_BROWSER_OVERRIDES := true
-GAPPS_PRODUCT_PACKAGES += GoogleContacts talkback LatinImeGoogle PrebuiltDeskClockGoogle CalculatorGoogle TagGoogle CalendarGooglePrebuilt Photos PrebuiltGmail
+GAPPS_PRODUCT_PACKAGES += GoogleContacts talkback LatinImeGoogle PrebuiltDeskClockGoogle CalculatorGoogle TagGoogle CalendarGooglePrebuilt Photos PrebuiltGmail MarkupGoogle
  $(call inherit-product, vendor/opengapps/build/opengapps-packages.mk)
  
